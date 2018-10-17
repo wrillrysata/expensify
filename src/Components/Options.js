f1dc5226a7c84667ab3526c105cd2fbab03b920a
Ijebusoma <vanessaosuka@gmail.com>
2018-10-17 11:29:30 +0100

    Modify components jsx

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -4,20 +4,23 @@ import Option from "./Option";
 
 const Options = props => (
   <div>
-    <div class="option-header">
-    <h3>Things to do</h3>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
+    <div className="widget-header">
+    <h3 className="widget-header__title">Things to do</h3>
+      <button className="button button--link"
+       onClick={props.handleDeleteOptions} >
+       Remove All
+       </button>
       </div>
-
-{props.options.length === 0 && <p>Add an option to get started</p>}
-      {props.options.map((option) => (
+{props.options.length === 0 && <p className="widget__message">Add an option to get started</p>}
+      {props.options.map((option, index) => (
         <Option 
+        count={index}
         key={option} 
         optionText={option} 
         handleDeleteOption = {props.handleDeleteOption}
         />
       ))}
-    </div>
+      </div>
   );
 
 export default Options;
