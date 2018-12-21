6b3780f707c96b177c7607e0bc665e9dc5e54d07
Ijebusoma <vanessaosuka@gmail.com>
2018-12-21 13:02:28 +0100

    [ft-editExpense] Wire up edit expense
    This commit implements
    edit expense feature
    
    [Ticket: #491]

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-const EditExpensePage = (props) => {
-    
-    return(
-
+import { connect } from "react-redux";
+import { editExpense, removeExpense } from "../actions/expenses";
+import ExpenseForm from "./ExpenseForm";
+const EditExpensePage = props => {
+  return (
     <div>
-        <p> Editing expense {props.match.params.id} </p>
-        </div>
-    );
-}
-    export default EditExpensePage
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push("/");
+        }}
+      />
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id })) &&
+            props.history.push("/");
+        }}
+      >
+        Remove{" "}
+      </button>
+    </div>
+  );
+};
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      expense => expense.id === props.match.params.id
+    ) //bug. Expense shows undefined
+  };
+};
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses'
+import { Link } from 'react-router-dom';
+import { removeExpense } from '../actions/expenses';
 const ExpenseListItem = ({dispatch, id,description, amount, createdAt}) => (
     <div>
+        <Link to = {`/edit/${id}`}>
      <h4>{description}</h4>
+     </Link>
      {amount} -  {createdAt}
-     <button onClick={()=>{
-        dispatch(removeExpense({ id }))
-     }}> 
-      Remove </button>
+     
 
         
     </div>
