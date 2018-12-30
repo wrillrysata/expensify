c8082493eaf0721380495d6d15cb4e381c094f12
Ijebusoma <vanessaosuka@gmail.com>
2018-12-30 21:29:34 +0100

    Chore - Add Redux devtool to workflow

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,7 +8,8 @@ export default () => {
     combineReducers({
       expenses: expenseReducer,
       filters: filterReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   return store;
 
